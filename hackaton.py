b6a1533c5e6811e3c91088ae20bedd50ce3e2839
PEPPACHAN <93025358+PEPPACHAN@users.noreply.github.com>
2021-10-24 08:34:20 +0300

    Update hackaton.py

--- hackaton.py
+++ hackaton.py
@@ -4,43 +4,49 @@ import csv
 #библиотеки
 
 
+URL = 'https://www.aviasales.ru/search/MOW2310AAQ29101?request_source=expired_search'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 OPR/80.0.4170.48', 'accept': '*/*'}
 HOST = 'https://www.aviasales.ru'
-URL = 'https://www.aviasales.ru/search/MOW2310AAQ29101?request_source=search_form.explore_history&expected_price_value=6270&expected_price_currency=rub&expected_price_source=history&payment_method=all'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 OPR/80.0.4170.48', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 TICKET = 'ticket.csv'
-#5 стр. обращение к сайту    6 стр. защита от проверки на бота
+
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
-#получение данных в html формате
+
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_='fade-enter-done')
-    price=[]
+    items = soup.find_all('div', class_='class')
+
+    price = []
     for item in items:
-        price.append({
-            'price': item.find('span', class_='price').get_text(strip=True),
-            'link': HOST + item.find('a', class_='buy-button  button').get('href')
-        })
-        return price
+             price.append({
+            'price': item.find('div', class_='class').get_text(strip=True),
+            'link': HOST + item.find('div', class_='class').get('href')
+             })
+    return price
 #поиск всех билетов
 
 def save_file(items, path):
     with open(path, 'w', newline='') as file:
-        writer = csv.writer(file, delattr(';'))
+        writer = csv.writer(file, delimiter=';')
         writer.writerow(['Цена', 'Ссылка'])
         for item in items:
-            writer.writerow(['price', 'link'])
+            writer.writerow([item['price'], item['link']])
 #сохрарение всего файла в эксель
 
 def parse():
+    URL = input('Введите URL: ')
+    URL = URL.strip()
     html = get_html(URL)
-    price = []
     if html.status_code == 200:
-        get_content(html.text)
+        price = []
         save_file(price, TICKET)
     else:
-        print('error')
+        print('Error')
+
+
+parse()
 #процесс поиска
+
